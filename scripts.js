b48f5c827a84ad3e774ff0cbaf2afcefbdd3f647
AndersonGamarraJW <andersong.v3105@gmail.com>
2022-12-31 19:23:38 -0500

    feat(calculator): Visualiza los numeros presionados en pantalla

---
+++ scripts.js
@@ -0,0 +1,18 @@
+let textWindow = '0';
+console.log(textWindow);
+
+const windowCalculator = document.querySelector('.window-calculator');
+windowCalculator.textContent = textWindow;
+const numberButton = document.querySelector('.number');
+const numbersButtons = document.querySelectorAll('.number');
+
+console.log(numberButton.value)
+console.log(windowCalculator.textContent);
+
+numbersButtons.forEach((buttonNumber)=>{
+    buttonNumber.addEventListener('click',(e)=>{
+        textWindow = textWindow + e.target.value;
+        console.log(textWindow);
+        windowCalculator.textContent = textWindow;
+    });
+});
